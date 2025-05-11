0048c5f2532e6f82678e82f100b6bca537be2645
Olga Mandrikova <mandrikovaolya@gmail.com>
2025-05-11 23:03:32 +0300

    Parameterized tests

--- src/test/java/ru/netology/services/VacationCounterTest.java
+++ src/test/java/ru/netology/services/VacationCounterTest.java
@@ -2,19 +2,31 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.CsvFileSource;
+
+
 
 public class VacationCounterTest {
 
-    @Test
+    @ParameterizedTest
+            @CsvFileSource(files="src/test/resources/Vacations.csv")
+   // @CsvSource({
+  //          "3, 10000, 3000, 20000",
+   //         "2, 100000, 60000, 150000"
+   // })
+
         //1
-    void shouldCalculateForTenThousands() {
+    void shouldCalculateForVacation(int expected, int income, int expenses, int threshold) {
         VacationCounter service = new VacationCounter();
 
         // подготавливаем данные:
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        long expected = 3;
+        //int income = 10_000;
+       // int expenses = 3_000;
+      //  int threshold = 20_000;
+        //int expected = 3;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
@@ -23,7 +35,7 @@ public class VacationCounterTest {
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
+ /*   @Test
         //2
     void shouldCalculateForOneHundredThousands() {
         VacationCounter service = new VacationCounter();
@@ -32,12 +44,12 @@ public class VacationCounterTest {
         int income = 100_000;
         int expenses = 60_000;
         int threshold = 150_000;
-        long expected = 2;
+        int expected = 2;
 
         // вызываем целевой метод:
         int actual = service.calculate(income, expenses, threshold);
 
         // производим проверку (сравниваем ожидаемый и фактический):
         Assertions.assertEquals(expected, actual);
-    }
+    }*/
 }
